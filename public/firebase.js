c37cbcc1b90a152f862696c32003fb81c15505a1
abhilash-kundoor <91517464+abhilash-kundoor@users.noreply.github.com>
2021-12-15 22:25:47 -0500

    Add files via upload

---
+++ public/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyA4gkA4IyXBkZUqWpvXIG9E9p1u0sgAB5Y",
+    authDomain: "prog8110finalproject.firebaseapp.com",
+    projectId: "prog8110finalproject",
+    storageBucket: "prog8110finalproject.appspot.com",
+    messagingSenderId: "12782065242",
+    appId: "1:12782065242:web:f0dbf98072077b7cd0aa63",
+    databaseURL: "https://prog8110finalproject-default-rtdb.firebaseio.com"
+  };
